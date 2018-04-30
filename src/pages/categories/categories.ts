7b456ba4a8fcf91591967931185c952459a818ef
Saud Bawazeer <saudb@giantpace.com>
2018-04-30 12:36:54 +0300

    Signed-off-by: Saud Bawazeer <saudb@giantpace.com>

--- src/pages/categories/categories.ts
+++ src/pages/categories/categories.ts
@@ -4,6 +4,7 @@ import { Http } from '@angular/http';
 
 // Custom
 import { Core } from '../../service/core.service';
+import { StorageMulti } from '../../service/storage-multi.service';
 
 // Page
 import { DetailCategoryPage } from '../detail-category/detail-category';
@@ -15,7 +16,7 @@ declare var google: any;
 @Component({
 	selector: 'page-categories',
 	templateUrl: 'categories.html',
-	providers: [Core,Geolocation]
+	providers: [StorageMulti,Core,Geolocation]
 })
 export class CategoriesPage {
 	@ViewChild('map') mapElement: ElementRef;
@@ -26,8 +27,8 @@ export class CategoriesPage {
 	parents:Object[] = [];
 	id:Number;
 	noResuilt:boolean = false;
-	areamap=[];
-	areaCoords=[];
+	public areamap=[];
+	marker:Marker;
 	vendorid=0;
 
 	constructor(
@@ -35,9 +36,50 @@ export class CategoriesPage {
 		private core: Core,
 		private navCtrl: NavController,
 		public platform: Platform,
+		private storageMul: StorageMulti,
 		private geolocation: Geolocation
 	){
 		core.showLoading();
+		this.areamap[58] = new google.maps.Polygon({
+		paths: [new google.maps.LatLng(21.41504803916235, 39.25614042871098),
+		new google.maps.LatLng(21.39949308208154, 39.346907556152246),
+		new google.maps.LatLng(21.314035940293635, 39.329978540039065),
+		new google.maps.LatLng(21.26940401302275, 39.31540914306629),
+		new google.maps.LatLng(21.25922089539485, 39.28844648681638),
+		new google.maps.LatLng(21.2553, 39.26769999999988),
+		new google.maps.LatLng(21.2504, 39.237699999999904),
+		new google.maps.LatLng(21.2543, 39.20649999999989),
+		new google.maps.LatLng(21.26529, 39.19249999999988),
+		new google.maps.LatLng(21.28625970522107, 39.175583703613256),
+		new google.maps.LatLng(21.3450975901386, 39.1808160263671),
+		new google.maps.LatLng(21.385787796792904, 39.19904689941404)],
+		strokeColor: "#00FF00",
+		strokeOpacity: 0.8,
+		strokeWeight: 3,
+		fillColor: "#00FF00",
+		fillOpacity: 0.35
+		});
+
+		this.areamap[59] = new google.maps.Polygon({
+		paths: [new google.maps.LatLng(21.440071522591765, 39.86615254609376),
+		new google.maps.LatLng(21.42611367070063, 39.88928509101561),
+		new google.maps.LatLng(21.4090824, 39.90291180000008),
+		new google.maps.LatLng(21.385885610961964, 39.916151546093715),
+		new google.maps.LatLng(21.36002995473438, 39.91665471015631),
+		new google.maps.LatLng(21.34363396852816, 39.895564900585896),
+		new google.maps.LatLng(21.324662178292847, 39.866251546093736),
+		new google.maps.LatLng(21.335598581019322, 39.8171987628906),
+		new google.maps.LatLng(21.361938381445487, 39.79770559882809),
+		new google.maps.LatLng(21.392179132364202, 39.787999080273494),
+		new google.maps.LatLng(21.426063065778216, 39.7970079533203),
+		new google.maps.LatLng(21.439896795992443, 39.82141863593756)],
+		strokeColor: "#00FF00",
+		strokeOpacity: 0.8,
+		strokeWeight: 3,
+		fillColor: "#00FF00",
+		fillOpacity: 0.35
+		});
+
 		let params = {cat_num_page:1, cat_per_page:100, parent: '0'};
 		let loadCategories = () => {
 			http.get(wordpress_url+'/wp-json/wooconnector/product/getcategories', {
@@ -62,76 +104,26 @@ export class CategoriesPage {
       zoom: 15,
       center: mylocation
     });
-	var marker = new google.maps.Marker({
+	this.marker = new google.maps.Marker({
 	 position: mylocation,
 	 map: this.map,
 	 draggable:true
   });
-
-	let updateVendor= (position)=>{
-			if( google.maps.geometry.poly.containsLocation(position, this.areamap[0]) ) {
-				this.vendorid = 58;
-				return;
+for (var key in this.areamap){
+	this.areamap[key].setMap(this.map);
+	if( google.maps.geometry.poly.containsLocation(this.marker.position, this.areamap[key]) ) {
+		this.vendorid = Number(key);
+		return;
 	}
-	if( google.maps.geometry.poly.containsLocation(position, this.areamap[1]) ) {
-			this.vendorid = 59;
-			return;
-	}
-this.vendorid = 0;
-console.log(this.vendorid);
-		}
-google.maps.event.addListener(this.map, 'drag', function(e) {
-//	marker.setPosition(this.map.getCenter());
+}
+this.map.addListener('center_changed', function() {
+	    // 3 seconds after the center of the map has changed, pan back to the
+	    // marker.
+	  });
+this.marker.addListener('dragend', this.updateVendor);
 
-});
-google.maps.event.addListener(marker, 'dragend', function(e) {
-updateVendor(e.latLng);
 
-});
-this.areaCoords[1] = [new google.maps.LatLng(21.41504803916235, 39.25614042871098),
-new google.maps.LatLng(21.39949308208154, 39.346907556152246),
-new google.maps.LatLng(21.314035940293635, 39.329978540039065),
-new google.maps.LatLng(21.26940401302275, 39.31540914306629),
-new google.maps.LatLng(21.25922089539485, 39.28844648681638),
-new google.maps.LatLng(21.2553, 39.26769999999988),
-new google.maps.LatLng(21.2504, 39.237699999999904),
-new google.maps.LatLng(21.2543, 39.20649999999989),
-new google.maps.LatLng(21.26529, 39.19249999999988),
-new google.maps.LatLng(21.28625970522107, 39.175583703613256),
-new google.maps.LatLng(21.3450975901386, 39.1808160263671),
-new google.maps.LatLng(21.385787796792904, 39.19904689941404)];
-this.areamap[0] = new google.maps.Polygon({
-paths: this.areaCoords[1],
-strokeColor: "#00FF00",
-strokeOpacity: 0.8,
-strokeWeight: 3,
-fillColor: "#00FF00",
-fillOpacity: 0.35
-});
-this.areamap[0].setMap(this.map);
-this.areaCoords[2] = [new google.maps.LatLng(21.440071522591765, 39.86615254609376),
-new google.maps.LatLng(21.42611367070063, 39.88928509101561),
-new google.maps.LatLng(21.4090824, 39.90291180000008),
-new google.maps.LatLng(21.385885610961964, 39.916151546093715),
-new google.maps.LatLng(21.36002995473438, 39.91665471015631),
-new google.maps.LatLng(21.34363396852816, 39.895564900585896),
-new google.maps.LatLng(21.324662178292847, 39.866251546093736),
-new google.maps.LatLng(21.335598581019322, 39.8171987628906),
-new google.maps.LatLng(21.361938381445487, 39.79770559882809),
-new google.maps.LatLng(21.392179132364202, 39.787999080273494),
-new google.maps.LatLng(21.426063065778216, 39.7970079533203),
-new google.maps.LatLng(21.439896795992443, 39.82141863593756)];
-this.areamap[1] = new google.maps.Polygon({
-paths: this.areaCoords[2],
-strokeColor: "#00FF00",
-strokeOpacity: 0.8,
-strokeWeight: 3,
-fillColor: "#00FF00",
-fillOpacity: 0.35
-});
-this.areamap[1].setMap(this.map);
-
-updateVendor(mylocation);
+this.updateVendor(this.marker.position);
   });
 
 		}
@@ -152,6 +144,17 @@ updateVendor(mylocation);
 			if(e['deltaX'] < 0) this.navCtrl.push(this.SearchPage);
 			else this.navCtrl.popToRoot();
 		}
+	}
+	updateVendor(position){
+	console.log(this.areamap);
+	for (var key in this.areamap){
+		if( google.maps.geometry.poly.containsLocation(this.marker.position, this.areamap[key]) ) {
+			this.vendorid = Number(key);
+			return;
+		}
+	}
+	this.vendorid = 0;
+	console.log(this.vendorid);
 	}
 
 
