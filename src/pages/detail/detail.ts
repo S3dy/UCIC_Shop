e6315b84b0ebffaed3a5fefd70aa113824faa39e
Saud Bawazeer <saudb@pace.sa>
2018-05-22 17:42:41 +0300

    Test update

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -72,7 +72,7 @@ export class DetailPage {
 	getData() {
 		// let params = { product_id: this.id };
 		this.core.showLoading();
-		console.log(this.lang,"lang"); 
+		console.log(this.lang,"lang");
 		this.http.get(wordpress_url + '/wp-json/wooconnector/product/getproduct/' + this.id+'?lang='+this.lang).subscribe(res => {
 			this.detail = res.json();
 			this.reviews_allowed = this.detail['reviews_allowed'];
